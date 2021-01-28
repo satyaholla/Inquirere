57873bab197d2a2c0a5d6f2abfa849c49cf15b83
satyaholla <62317901+satyaholla@users.noreply.github.com>
2021-01-28 10:11:18 -0600

    Update LogoutBar.tsx

--- client/src/components/LogoutBar.tsx
+++ client/src/components/LogoutBar.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { GoogleLogout } from 'react-google-login';
 
-import "./Logout.css"
-
 //TODO(weblab student): REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "1069618540025-j0mmu098u8d0s57nvtpsr1fcvahvdvld.apps.googleusercontent.com";
 
